fdecff3a86fba7f537f33703eac5a58b15334fe6
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2019-01-01 19:54:46 -0200

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080" // é a url básica do Spring Boot
+    baseUrl: "http://localhost:8080", // é a url básica do Spring Boot
+    bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-alexander"
 }
