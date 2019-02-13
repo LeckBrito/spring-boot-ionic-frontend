3a6621a9e71e8cc9dac6aa192ae9afe4ab49f981
Alexander Dias Brito <alexander.diasbrito@gmail.com>
2019-02-12 22:46:48 -0200

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){        
+    constructor(public storage: StorageService, public alterCtrl: AlertController){        
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -26,9 +27,17 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
             
             switch(errorObj.status){
+
+                case 401:
+                    this.handle401()
+                    break;
+
                 case 403:
                     this.handle403();
                     break;
+
+                default:
+                    this.handleDefaultError(errorObj);
             }
 
             return Observable.throw(errorObj);//propagar o erro para o controlador que faz a requisição...
@@ -38,10 +47,37 @@ export class ErrorInterceptor implements HttpInterceptor {
         }) as any;
     }
 
+    handle401(){
+        let alert = this.alterCtrl.create({
+            title: 'Erro 401; falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,//é opcional, mas isso não deixa clicar fora do alert, obriga a apertar algum botão
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
     handle403(){
         this.storage.setLocalUser(null);
     }
 
+    handleDefaultError(errorObj){
+        let alert = this.alterCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,//é opcional, mas isso não deixa clicar fora do alert, obriga a apertar algum botão
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
 }
 
 export const ErrorInterceptorProvider = {
